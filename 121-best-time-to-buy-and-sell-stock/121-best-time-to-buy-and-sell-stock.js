7c3576824ba207dae5e8f3b41c65111ca69a74d3
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-08-19 12:22:07 -0400

    Time: 130 ms (46.31%), Space: 51.7 MB (69.49%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let maxProfit = -Infinity;
+    let min = prices[0];
+    let difference;
+    
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < min) {
+            min = prices[i];
+            continue;
+        }
+        difference = prices[i] - min;
+        if (difference > maxProfit) maxProfit = difference;
+    };
+    
+    if (maxProfit < 0) {
+        return 0;
+    } else {
+        return maxProfit;
+    }
+};
