452b2ff87c90a6ca5006e0b10b963235e62bd36b
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-08-21 16:58:14 -0400

    Time: 92 ms (77.82%), Space: 45.2 MB (26.40%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -21,5 +21,18 @@ var isIsomorphic = function(s, t) {
     }
     
     return Object.values(hash).length === Object.keys(hash).length;
+    
+    let obj1 = {};
+    let obj2 = {};
+    
+    for (let i = 0; i < s.length; i++){
+        if (obj1[s[i]] !== obj2[t[i]]){
+            return false;
+        } else {
+            obj1[s[i]] = i;
+            obj2[t[i]] = i;
+        }
+    }
+    return true;
 };
 
