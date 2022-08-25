1b07ba909f07d836f3493a8a5bc5033985bff17f
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-08-25 18:01:50 -0400

    Time: 98 ms (51.64%), Space: 42 MB (44.56%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,10 +3,9 @@
  * @return {number}
  */
 var fib = function(n, memo = {}) {
+    if (n === 1 || n === 0) return n;
     if (n in memo) return memo[n];
-    if (n === 0) return 0;
-    if (n === 1) return 1;
     
-    memo[n] = fib(n - 1) + fib(n - 2);
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
     return memo[n];
 };
