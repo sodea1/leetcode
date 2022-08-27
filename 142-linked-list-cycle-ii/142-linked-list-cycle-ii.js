7207d0a5da8404203689b827549f7970b191dcd3
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-08-27 17:40:10 -0400

    Time: 84 ms (90.71%), Space: 45.6 MB (12.37%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let visited = new Set();
+    let node = head;
+    
+    while (node) {
+        if (visited.has(node)) return node;
+        visited.add(node);
+        node = node.next;
+    }
+    
+    return null;
+};
