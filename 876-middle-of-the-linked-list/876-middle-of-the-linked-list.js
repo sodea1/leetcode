aced3a583965f5d812fc13151f8f4f75ca9f9b53
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-08-27 17:31:08 -0400

    Time: 115 ms (8.75%), Space: 41.9 MB (74.29%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+//           S         F     
+// 1 => 2 => 3 => 4 => 5
+//                                     F
+// 1 => 2 => 3 => 4 => 5 => 6
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+    
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    return slow;
+};
