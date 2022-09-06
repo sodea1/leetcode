9e770b06e618a816e074c6937226ba98113a987f
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-09-05 21:19:26 -0400

    Time: 100 ms (44.01%), Space: 42.4 MB (62.80%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -5,7 +5,7 @@
 // flower, flow, flight
 var longestCommonPrefix = function(strs) {
     if (strs.length === 0) return "";
-    let prefix = strs[0]; // flower
+    let prefix = strs[0]; // flower, flowe, flow, flo, fl
     for (let i = 1; i < strs.length; i++) {
         while (strs[i].indexOf(prefix) !== 0) {
             prefix = prefix.slice(0, prefix.length - 1);
