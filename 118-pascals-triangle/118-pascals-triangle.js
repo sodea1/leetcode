bc4801a476e228f4f85ed1b31a9a018fe518f7ef
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-09-17 06:58:18 -1000

    Time: 97 ms (30.90%), Space: 41.8 MB (82.53%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+// [1]
+// [1, 1]
+// [1, 2, 1]
+// [1, 3, 3, 1]
+// [1, 4, 6, 4, 1]
+
+var generate = function(numRows) {
+    const pascal = []; // [[1], ]
+    
+    for (i = 0; i < numRows; i++) {
+        const row = [];
+        row.push(1);
+        
+        for (j = 1; j < i; j++) { 
+            const above = (pascal[i - 1][j] !== null) ? pascal[i - 1][j] : 0;
+            const left = (pascal[i - 1][j - 1] !== null) ? pascal[i - 1][j - 1] : 0;
+            row[j] = above + left;
+        }
+        if (i !== 0) row.push(1);
+        pascal.push(row);
+    }
+    
+    return pascal;
+};
+
+// let output = [[1]];
+//     for (let i = 0; i < numRows - 1; i++) {
+//         let x = output[output.length - 1];
+//         let row = [1];
+//         for (let j = 1; j < x.length; j++) {
+//             row.push(x[j]+x[j-1])
+//         }
+//         row.push(1);
+//         output.push(row);
+//     }
+//     return output;
