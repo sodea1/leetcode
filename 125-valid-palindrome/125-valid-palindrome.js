c28feaf80ee3da4993e641f26a3fd067d07e9288
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-09-17 08:09:50 -1000

    Time: 111 ms (55.29%), Space: 44.6 MB (68.61%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,23 +3,13 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let noSpaces = "";
-    const alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const alphanum = s.replace(/[^a-zA-Z\d]/g, "").toLowerCase();
     
-    for (let char of s) {
-        if (alpha.includes(char.toLowerCase())) {
-            noSpaces += char.toLowerCase();
-        }
+    let left = 0;
+    for (let right = alphanum.length - 1; right > -1; right--) {
+        if (alphanum[left] !== alphanum[right]) return false;
+        left++;
     }
-    return (noSpaces === "") ? true : checkLetters(noSpaces);
-};
-
-const checkLetters = (str) => {
-    let i = 0;
     
-    for (let j = str.length - 1; j > -1; j--) {
-        if (i >= j && str[i] === str[j]) return true;
-        if (str[i] !== str[j]) return false; 
-        i++;
-    }
+    return true;
 };
