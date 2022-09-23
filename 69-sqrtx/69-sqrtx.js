414d6e73f5ee467bc448f40ade2f7e74e7b2c84c
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-09-23 09:59:11 -1000

    Time: 135 ms (37.87%), Space: 44.1 MB (27.46%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -2,6 +2,10 @@
  * @param {number} x
  * @return {number}
  */
+
+// x = 17 ~ 4.08
+//    l  r            
+// 2, 3, 4, 5, 6, 7, 8
 var mySqrt = function(x) {
     if (x < 2) return x;
     
@@ -9,16 +13,16 @@ var mySqrt = function(x) {
     let right = Math.floor(x / 2);
     
     while (left <= right) {
-        let pivot = Math.floor((left + right) / 2);
+        let pivot = Math.floor(left + (right - left) / 2);
         let num = pivot * pivot;
-        
         if (num > x) {
             right = pivot - 1;
         } else if (num < x) {
             left = pivot + 1;
         } else {
             return pivot;
-        }
-    }
+        };
+    };
+    
     return right;
 };
