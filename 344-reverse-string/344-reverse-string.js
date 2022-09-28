53a80cd71d7f57c88861f9fb87f074a71d19ba88
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-09-28 08:55:30 -1000

    Time: 179 ms (16.45%), Space: 49.4 MB (46.06%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let p1 = 0;
+    let p2 = s.length - 1;
+    
+    while (p1 < p2) {
+        [s[p1], s[p2]] = [s[p2], s[p1]];
+        p1++;
+        p2--;
+    };
+    
+    return s;
+};
