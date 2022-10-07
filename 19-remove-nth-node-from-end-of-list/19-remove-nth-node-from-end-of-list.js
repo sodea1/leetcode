90194ec43e5543ac81496c2433e1f3ffe763f91c
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-10-07 11:08:10 -0400

    Time: 102 ms (47.49%), Space: 43.6 MB (22.13%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.js
@@ -10,39 +10,63 @@
  * @param {number} n
  * @return {ListNode}
  */
-//           *    
-// 1 -> 2 -> 3 => 4 => 5
-// length - n = x representing number of nodes from head
-// 
-// h        rem              
-// 4 => 5 => 6 -> 7 => 8 => 9
-// n = 4
+// Sample Problem:
+//       i                   
+//                                               j                        
+// 1 -> 2 -> 3 => 4 => 5 => 6 => 7 => 8 => 9 => 10
+//                x
+// n = 0
 
-// [1]
-// n = 1
-
-var removeNthFromEnd = function(head, n) {
-    let length = 0;
-    let curr = head;
-    while (curr !== null) {
-        curr = curr.next;
-        length++;
+var removeNthFromEnd = function(head, n) { // n = 7
+    let currI = head;
+    let currJ = head;
+    
+    while (currJ.next !== null) {     
+        if (n > 0) {
+            currJ = currJ.next;
+            n--;
+        } else if (currJ.next !== null) {
+            currI = currI.next;
+            currJ = currJ.next;
+        };
     };
     
-    // should have correct length of list
+    // remove first node
+    if (n === 1) return head.next;
     
-    // find distance (x) from head to target node
-    let x = length - n; // 1
-    if (x < 1) return head.next;
-    
-    let curr2 = head;
-    while (x > 1) {
-        curr2 = curr2.next;
-        x--; 1
+    // remove last node
+    if (currI.next === currJ) {
+        currI.next = currJ.next;
+        return head;
     };
     
-    // curr2 = node(5)
-    
-    curr2.next = curr2.next.next;
+    // remove middle node
+    currI.next = currI.next.next;
     return head;
+    
+    
+    
+//     let length = 0;
+//     let curr = head;
+//     while (curr !== null) {
+//         curr = curr.next;
+//         length++;
+//     };
+    
+//     // should have correct length of list
+    
+//     // find distance (x) from head to target node
+//     let x = length - n; // 1
+//     if (x < 1) return head.next;
+    
+//     let curr2 = head;
+//     while (x > 1) {
+//         curr2 = curr2.next;
+//         x--; 1
+//     };
+    
+//     // curr2 = node(5)
+    
+//     curr2.next = curr2.next.next;
+//     return head;
 };
