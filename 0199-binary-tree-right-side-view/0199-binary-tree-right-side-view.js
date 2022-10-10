db6cc9d7b7f1a8ce6eacf8551f77049f9b1c89cf
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-10-10 13:08:13 -0400

    Time: 138 ms (5.09%), Space: 43.4 MB (92.19%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+// input: tree's root node
+// output: array of values []
+
+//       1
+//.   2    3
+//. 4  5  6    
+
+// output = [1,    2, 3, null, 5, null, null];
+
+// queue = [[2, 3]]; => [3, null, 5]
+// curr = Node(1);
+
+// shift 1
+// add 1 to output
+// add 1's left child
+// add 1's right child
+
+var rightSideView = function(root) {
+    if (root === null) return [];
+    const vals = [root.val];
+    const queue = [root]; // [ [2, null] ]
+    
+    while (queue.length > 0) {
+        const node = queue.shift(); // [ 2, 3 ]
+        let layer = [];
+        if (node === root) {
+            const leftNode = node.left;
+            const rightNode = node.right;
+            if (leftNode) layer.push(leftNode);
+            if (rightNode) layer.push(rightNode);
+            if (layer.length > 0) queue.push(layer);
+        } else {
+            vals.push(node[node.length - 1].val)
+            while (node.length > 0) { // [2, 3]
+                const dog = node.shift(); // 2
+                if (dog.left) layer.push(dog.left);
+                if (dog.right) layer.push(dog.right);
+            };
+            if (layer.length > 0) queue.push(layer);
+        };
+    };
+    
+    return vals;
+};
