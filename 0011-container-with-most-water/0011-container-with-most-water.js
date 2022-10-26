e501cec0ac48d12258a34122abe69da657d40bfb
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-10-26 14:22:31 -0400

    Time: 129 ms (51.38%), Space: 49.4 MB (79.99%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,69 +3,23 @@
  * @return {number}
  */
 
-// currWidth = i - j;
-// currHeight = Math.max(i, j)
-// must check every subarray of length 2 or more
-//             i            
-//                      j  
-//      i:  0  1  2  3  4
-// inputs: [2, 5, 66, 64, 3], n = 5
-// line =>  x axis: (i, 0) ; y axis (i, height[i])
+//
+// 
 
-//   
-//      5   
-//      |  4
-//      |  |     3
-//   2  |  |     |
-//   |  |  |  1  |
-//   |  |  |  |  |
-// 0    
-
-// obj { height: width, h2: w2, ... }
-                            //   i:  0 1 2 3 4 5 6 7 8
 var maxArea = function(height) { // [1,8,6,2,5,4,8,3,7]
-    let maxArea = 0;
-    let start = 0;
-    let end = height.length - 1;
     
-    while (start !== end) {
-        // calc width
-        let width = end - start;
+    let maxArea = 0;
+    let i = 0;
+    let j = height.length - 1;
+    
+    while (i < j) {
+        // calculate area of current container
+        let area = (j - i) * Math.min(height[i], height[j]);
+        maxArea = (area > maxArea) ? area : maxArea;
         
-        // calc ht
-        let ht = Math.min(height[start], height[end]);
-        
-        if (ht === height[start]) {
-            start++;
-        } else {
-            end--;
-        };
-        
-        let area = width * ht;
-        if (maxArea < area) maxArea = area;
+        // increment/decrement based on comparison of each current wall height
+        (height[i] <= height[j]) ? i++ : j--;
     };
     
     return maxArea;
 };
-
-
-// inefficient O(n^2)
-// let maxArea = 0;
-    
-//     for (let i = 0; i < height.length; i++) {
-//         for (let j = i + 1; j < height.length; j++) {
-//             // calculate width
-//             let width = j - i; // 1
-            
-//             // calculate height
-//             let ht = Math.min(height[j], height[i]); // 2
-                
-//             // find total area
-//             let area = width * ht; // 2
-            
-//             // check total area against maxArea 
-//             if (area > maxArea) maxArea = area;
-//         };
-//     };
-    
-//     return maxArea;
