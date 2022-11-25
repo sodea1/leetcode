2de2fb37da10e942d44ffc91bbd3ea30c4e57d56
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-11-25 11:50:42 -0500

    Time: 146 ms (90.69%), Space: 52.7 MB (60.95%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -10,43 +10,45 @@
 
 
 var threeSum = function(nums) { 
-    nums = nums.sort((a, b) => a - b);          
-    const triplets = [];
+    nums.sort((a, b) => a - b);
+    const triplets = []; // triplet combos non unique that sum to zero
     
-    for (let i = 0; i < nums.length; i++) {   // [-1,0,1,2,-1,-4,-2,-3,   3,0,4]
+    for (let i = 0; i < nums.length; i++) {
         if (nums[i] > 0) {
             break;
         };
     
-        if (i === 0 || nums[i - 1] !== nums[i]) {
+        if (nums[i - 1] !== nums[i]) {
             twoSumII(nums, i, triplets);
-        };
+        };        
     };
     
     return triplets;
-    
 };
 //                              triplets = [[-4, 0, 4], [-4, 1, 3], [-3, -1, 4], [-3, 0, 3]]
-                                          //                                        h
-                                          //         i
-                                          //                        l
-const twoSumII = (nums, i, triplets) => {   // [-4, -3, -2, -1, -1, 0,  0,  1,  2,  3, 4]
+                                          //               h
+                                          //           i
+                                          //             l
+const twoSumII = (nums, i, triplets) => {   // [-4,-1,-1,0,1,2]
     let lo = i + 1;
     let hi = nums.length - 1;
     
+    let sum;
     while (lo < hi) {
-        let sum = nums[i] + nums[lo] + nums[hi];
-        if (sum === 0) {
-            triplets.push([ nums[i], nums[lo], nums[hi] ]);
+        sum = nums[i] + nums[lo] + nums[hi];
+        if (sum < 0) {
+            // increment lo
             lo++;
+        } else if (sum > 0) {
+            // decrement hi
             hi--;
+        } else {
+            triplets.push([nums[i], nums[lo], nums[hi]]);
+            hi--;
+            lo++;
             while (lo < hi && nums[lo] === nums[lo - 1]) {
                 lo++;
             }
-        } else if (sum > 0) {
-            hi--;
-        } else {
-            lo++;
         }
-    };
+    }
 };
