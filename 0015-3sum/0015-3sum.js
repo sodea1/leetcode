c1ad767e42870e26aeaa3b57142467cadfd7a1d9
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-11-25 12:34:08 -0500

    Time: 548 ms (21.51%), Space: 59.5 MB (17.35%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -18,37 +18,67 @@ var threeSum = function(nums) {
             break;
         };
     
-        if (nums[i - 1] !== nums[i]) {
-            twoSumII(nums, i, triplets);
-        };        
+        // must call twoSumII for every UNIQUE number in nums
+        if (i === 0 || nums[i - 1] !== nums[i]) {
+            twoSum(nums,i,triplets);
+            // twoSumII(nums, i, triplets);
+        };  
     };
     
     return triplets;
 };
-//                              triplets = [[-4, 0, 4], [-4, 1, 3], [-3, -1, 4], [-3, 0, 3]]
-                                          //               h
-                                          //           i
-                                          //             l
-const twoSumII = (nums, i, triplets) => {   // [-4,-1,-1,0,1,2]
-    let lo = i + 1;
-    let hi = nums.length - 1;
-    
-    let sum;
-    while (lo < hi) {
-        sum = nums[i] + nums[lo] + nums[hi];
-        if (sum < 0) {
-            // increment lo
-            lo++;
-        } else if (sum > 0) {
-            // decrement hi
-            hi--;
-        } else {
-            triplets.push([nums[i], nums[lo], nums[hi]]);
-            hi--;
-            lo++;
-            while (lo < hi && nums[lo] === nums[lo - 1]) {
-                lo++;
+//                           hi
+//                        lo
+//       i
+// [-4, -3, -3, -1, 0, 1, 1, 2, 2, 4]
+                                         // seen = [-1, 0, ]
+                                        //               
+                                        //               j
+                                       //    i        
+const twoSum = (nums, i, triplets) => { // [-4,-1,-1,0,1,5] => [[-4, 5, 1]]
+    // check if lo and hi sum to the target
+    let seen = new Set();
+    let j = i + 1;
+
+    while (j < nums.length) {
+        let complement = -nums[i] - nums[j];
+        if (seen.has(complement)) {
+            triplets.push([nums[i], nums[j], complement]);
+            while (j + 1 < nums.length && nums[j] === nums[j + 1]) {
+                j += 1;
             }
         }
-    }
-};
+        
+        seen.add(nums[j]);
+        j++;
+    };
+}
+
+//                              triplets = [[-4, 0, 4], [-4, 1, 3], [-3, -1, 4], [-3, 0, 3]]
+                                          //               h
+                                          //     i
+                                          //             l
+// const twoSumII = (nums, i, triplets) => {   // [-4,-1,-1,0,5,5]
+//     let lo = i + 1;
+//     let hi = nums.length - 1;
+    
+//     let sum;
+//     while (lo < hi) {
+//         sum = nums[i] + nums[lo] + nums[hi];
+//         if (sum < 0) {
+//             // increment lo
+//             lo++;
+//         } else if (sum > 0) {
+//             // decrement hi
+//             hi--;
+//         } else {
+//             triplets.push([nums[i], nums[lo], nums[hi]]);
+//             hi--;
+//             lo++;
+//             // 
+//             while (lo < hi && nums[lo] === nums[lo - 1]) {
+//                 lo++;
+//             }
+//         }
+//     }
+// };
