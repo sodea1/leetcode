4170a9fca8a1a69f76682ad1bd23c85754793220
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-11-30 15:52:56 -0500

    Time: 102 ms (31.98%), Space: 42.5 MB (24.79%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.js
@@ -39,7 +39,7 @@ var reverseBetween = function(head, left, right) {
         curr = thirdNode;
         right--;
     };
-    console.log(curr)
+    
     if (connector) {
         connector.next = prev;
     } else {
@@ -50,37 +50,3 @@ var reverseBetween = function(head, left, right) {
     
     return head;
 }
-
-
-
-
-                  // [... [leftNode: 3,2, rightNode: 4,3,2], [leftNode: 3,4,2, rightNode: 4,2], [leftNode: 2,3,4,5, rightNode: 5]]
- 
-// var reverseBetween = function(head, left, right) {
-//     let rightNode = head;
-//     let leftNode = head;
-//     let stop = false;
-    
-//     const recurse = (rightNode, left, right) => {
-//         if (right === 1) return;
-        
-//         rightNode = rightNode.next;
-//         if (left > 1) {
-//             leftNode = leftNode.next;
-//         }
-        
-//         recurse(rightNode, left - 1, right - 1);
-        
-//         if (leftNode === rightNode || rightNode.next === leftNode) {
-//             stop = true;
-//         };
-        
-//         if (!stop) {
-//             [leftNode.val, rightNode.val] = [rightNode.val, leftNode.val];
-//             leftNode = leftNode.next;
-//         }
-//     }
-    
-//     recurse(rightNode, left, right);
-//     return head;
-// };
