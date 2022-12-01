045354e89788c3811602814935e575267483d85b
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-12-01 13:52:43 -0500

    Time: 76 ms, Memory: 41.9 MB - LeetHub

---
+++ reverse-linked-list-ii/reverse-linked-list-ii.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+// right = 3
+// left = 1
+//  p
+//    c      
+// [1,2,3,4,5]
+//.     3
+
+
+var reverseBetween = function(head, left, right) {
+    let prev = null;
+    let curr = head;
+    
+    while (left > 1) {
+        prev = curr;
+        curr = curr.next;
+        left--;
+        right--;
+    }
+    
+    let connector = prev;
+    let tail = curr;
+    
+    while (right > 0) {
+        let thirdNode = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = thirdNode;
+        right--;
+    };
+    
+    if (connector) {
+        connector.next = prev;
+    } else {
+        head = prev;
+    };
+    
+    tail.next = curr;
+    
+    return head;
+}
