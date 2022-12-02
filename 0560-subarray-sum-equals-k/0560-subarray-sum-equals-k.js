67005c6a738a1ab41c2925b8ecb72d424b4de28b
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-12-02 17:02:18 -0500

    Time: 244 ms (5.19%), Space: 67.5 MB (5.28%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+// { 0:1, 1:1, 3:1, 4:1, 6:1, 7:1 }
+//.  
+// currRunningSum = 3 where (3 - k) = 0
+//           *
+// [1, 2, 1, 2, 1]
+// k = 3
+
+var subarraySum = function(nums, k) {
+    const map = new Map([[0, 1]]);
+    let runningSum = 0;
+    let count = 0;
+    
+    for (let num of nums) {
+        runningSum += num;
+        count += map.get(runningSum - k) || 0;
+        map.set(runningSum, map.get(runningSum) + 1 || 1);
+    };
+    console.log(map)
+    return count;
+};
