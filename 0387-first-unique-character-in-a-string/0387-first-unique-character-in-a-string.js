531d59a4c81419b64f5558771e6f9756a7e4057e
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-12-02 15:45:25 -0500

    Time: 103 ms (95.10%), Space: 44.7 MB (90.81%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -2,14 +2,15 @@
  * @param {string} s
  * @return {number}
  */
+// Approach:
+// "inebriated"
 
 var firstUniqChar = function(s) {
-    if (s.length === 1) return 0;
-    
     for (let i = 0; i < s.length; i++) {
         if (s.indexOf(s[i]) === i && s.indexOf(s[i], i + 1) === -1) {
             return i;
-        }
-    }
+        };
+    };
+    
     return -1;
 };
