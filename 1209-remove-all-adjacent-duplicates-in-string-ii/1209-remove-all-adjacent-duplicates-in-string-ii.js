f23458bc97f66ba4d40fe27d3bc2939d18bf6fee
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-12-05 14:47:43 -0500

    Time: 93 ms (91.20%), Space: 50.8 MB (53.60%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -3,32 +3,35 @@
  * @param {number} k
  * @return {string}
  */
-// [d, 1], [e, 3], 
-// sub = eee
-//       *
-// s = "deeedbbcccbdaa"
-// k = 3
+
+
+// "leetcodee" 2
+// "ltcode"
+       
+// "billybobbyjooj"
+// "biybyjj"
+// "biyby"
+
 
 var removeDuplicates = function(s, k) {
     const stack = [];
     
     for (let char of s) {
         if (stack.length && stack[stack.length - 1][0] === char) {
-            stack[stack.length - 1][1] += 1;
+            stack[stack.length - 1][1]++;
             if (stack[stack.length - 1][1] === k) {
                 stack.pop();
             };
         } else {
             stack.push([char, 1]);
         }
-    };
-    
-    let res = "";
-    
-    console.log(stack)
-    for (let [char, count] of stack) {
-        res += char.repeat(count);
     }
     
-    return res;
+    let collapsedStr = "";
+    
+    for (let [char, count] of stack) {
+        collapsedStr = collapsedStr + char.repeat(count);
+    };
+    
+    return collapsedStr;    
 };
