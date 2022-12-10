690cf95ae8a04ce09efc45d839d9f292bfaafa90
Sean O'Dea <40174573+sodea1@users.noreply.github.com>
2022-12-10 15:46:53 -0500

    Time: 91 ms (68.52%), Space: 41.8 MB (96.67%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.js
+++ 1029-two-city-scheduling/1029-two-city-scheduling.js
@@ -2,25 +2,13 @@
  * @param {number[][]} costs
  * @return {number}
  */
-//                  1th
-// costs = [[a,b], [a,b]...]
-// a and b represent different costs to fly to different cities
-// [[20, 50], [50, 70], [100, 20], [200, 80]];
-//     -30       -20       +80.        +120
-
 var twoCitySchedCost = function(costs) {
-    let totalCosts = 0;
-    costs.sort((a, b) => (a[0] - a[1]) - (b[0] - b[1]));
+    costs.sort((a, b) => (b[1] - b[0]) - (a[1] - a[0]));
+    let minCost = 0;
     
     for (let i = 0; i < costs.length; i++) {
-        if (i < Math.floor(costs.length / 2)) {
-            // take a
-            totalCosts += costs[i][0];
-        } else {
-            // take b
-            totalCosts += costs[i][1];
-        }
-    }
+        (i < costs.length / 2) ? minCost += costs[i][0] : minCost += costs[i][1];
+    };
     
-    return totalCosts;
+    return minCost;
 };
